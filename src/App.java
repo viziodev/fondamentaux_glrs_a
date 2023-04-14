4785dbf7d45a2802792168d5f56f95d46d81d2e7
viziodev <viziodev03@gmail.com>
2023-04-14 17:03:50 +0000

    Cours N 2 Java

--- src/App.java
+++ src/App.java
@@ -1,117 +1,126 @@
 import java.util.Scanner;
 
 public class App {
+   //Enumeration a Revenir
+    private static Scanner scanner=new Scanner(System.in);//Attributs  double[] notes;// double notes[]
+    private final static int N=10;//Taille de Reservation
+    private static double[] notes=new double[N] ;
+    private static int nbreNote;
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        /*
-         * Types Primitifs => Types Valeurs
-         * int,long, short, char , boolean,void
-         *  Reels=> float, double(defaut)
-         */
-         // int x=2;
-         // double y=12.5;
-         // float z=12.5f;
-          //Constantes 
-          final double PI=3.14;
-        /**
-         * Classes=> Attributs + methodes
-         * Convention Ecriture d'une Classe => PascalCase
-         * MaPremiereClasseJava
-         * JDK
-         *  Chaines => String  => java.lang
-         *  Environnement=>System
-         *    System.in => Clavier
-         *    System.out=>Ecran
-         *  Classe de Saisie =>Scanner
-         *     Methodes  => CamelCase  
-         *      nextInt():Lire un entier
-         *      nextDouble()
-         *      nextFloat()
-         *      nextBoolean()
-         *      nextLine(): Lire une chaine de plusieurs mots
-         *      next(): Lire une chaine d'un mot
-         * Creer Par Programmeurs => UML
-            * Description de Donnees  => entites(DC)
-            * Services  => servives ou metiers(DUC)
-            * Vues => Affichage et Saisie
-            * Repository  => models => Classes de Persistences ou Accees 
-            *                aux donnees
-          Etapes pour Manipuler une Classe
-             1. Declaration un objet de la Classe
-             2. Instanciation ou creation de l'objet
-             3. Donner un etat a l'etat  
-         */
-        String chaine ;//Chaine
-        //1-Declaration de l'objet
-        Scanner scanner;
-        scanner=new Scanner(System.in);
-       /*  int x;
-        double y,s;
-        System.out.println("Entrer un Entier");
-        x=scanner.nextInt();
-        System.out.println("Entrer un Reel");
-        y=scanner.nextDouble();
-        s=x+y;
-        System.out.println("La Somme  est "+s);
-        System.out.println("La Somme  est "+(x+y));
-        System.out.println(s+"est la Somme ");*/
-        int x;
-        double y;
-        do {
-            System.out.println("Entrer un Entier");
-            x=scanner.nextInt();
-        } while (x<=0);
-        
-        System.out.println("Entrer un Reel");
-        y=scanner.nextDouble();
-        if(x>y){
-            System.out.println(x+" , "+y);   
-        }else{
-            System.out.println(y+" , "+x);  
-        }
      
+         
+             
 
-        //Tableaux  => Classe
-        /*
-         * type[]  => Tableau
-         * 1-Declation
-         * int[] t;  double[] f;
-         * String[] ch;
-         * 2-Instanciation
-         *  type[] tab=new  type[N] ;
-         *  int[] t=new int[10];
-         *  String[] ch=new String[5];
-         */
+               int choix;
+        do {
+           
+            System.out.println("1. Saisir  un tableau de N notes");   
+            System.out.println("2. Afficher le tableau de notes");   
+            System.out.println("3. Afficher la note Min et Note Max");    
+            System.out.println("4. Modifier un note a une Position i");    
+            System.out.println("5. Trier le Tableau de Notes par ordre Croissant");   
+            System.out.println("6. Supprimer un note a une Position i");   
+            System.out.println("7. Ajouter un note a une Position i");   
+            System.out.println("8. Tranferer dans un tableau toutes les notes >=10");   
+            System.out.println("9.Quitter"); 
+            System.out.println("Faites votre choix"); 
+            choix=scanner.nextInt();
 
-         int[] t;
-         final int N=5;
-         t=new  int[N];
-        
-         for (int index = 0; index <N ; index++) {
-            System.out.println("Entrer un Entier");
-            t[index] =scanner.nextInt();
-         }
-         for (int index = 0; index <N ; index++) {
-            if(t[index]%2==0){
-                System.out.println(t[index]);
+            switch (choix) {
+                case 1:
+                   do{
+                          System.out.println("Entrer le nombre de notes");
+                          nbreNote=scanner.nextInt();
+                   }while(nbreNote<0 || nbreNote>N);
+                   saisieTableau();
+
+                break;
+                case 2:
+                    afficheTableau();
+                break;
+                case 3:
+                    afficheNoteMinEtMax();
+                break;
+                 case 4 : 
+                 modfierNoteAUnePos();
+                 afficheTableau();
+                 break;
+                 case 8 : 
+                 tranfereNoteSupMoy();
+                 break;
+                default:
+                    break;
             }
-         }
-         /*
-          * Menu Suivant
-              1. Saisir  un tableau de N notes
-              2. Afficher le tableau de notes
-              3. Afficher la note Min et Note Max
-              4. Modifier un note a une Position i
-              5. Trier le Tableau de Notes par ordre Croissant
-              6. Supprimer un note a une Position i
-              7. Ajouter un note a une Position i
-              8. Tranferer dans un tableau toutes les notes >=10
-              9.Quitter
-              NB : 
-                1. 0<=Note<=20
-          */
+        } while (choix!=9);
+         
+      
+       
+    }
 
+    public static double isNote(){
+        double note;
+        do {
+            System.out.println("Entrer une note");
+            note=scanner.nextDouble();
+            if(note<0 || note>20)
+              System.out.println("Note comprise entre 0 et 20");
+        } while (note<0 || note>20); 
+        return note;
+    }
 
-         scanner.close();
+   public  static void saisieTableau(){
+        for (int index = 0; index < nbreNote; index++) {
+               notes[index]=isNote();
+        }
+    }
+
+    public  static void afficheTableau(){
+        for (int index = 0; index < nbreNote; index++) {
+             System.out.print(notes[index]+"|");        
+        }
+        System.out.println(""); 
+    }
+
+    public  static void afficheNoteMinEtMax(){
+        double min=20,max=0;
+
+        for (int index = 0; index < nbreNote; index++) {
+                if(min>notes[index]) {
+                    min=notes[index];
+                }  
+                if(max<notes[index]) {
+                    max=notes[index];
+                }     
+        }
+
+          System.out.println("Note Max : "+max); 
+          System.out.println("Note Min : "+min);        
+   
+        
+    }
+
+    public  static void modfierNoteAUnePos(){
+        int pos;
+        do{
+            System.out.println("Entrer la Position");
+            pos=scanner.nextInt();
+       }while(pos<0 || pos>nbreNote);
+
+        double note=isNote();
+        notes[pos]=note;
+        System.out.println(""); 
+    }
+
+    public  static void tranfereNoteSupMoy(){
+        double noteSupMoy[]=new double[nbreNote];
+        int nbreVal=0;
+        for (int index = 0; index < nbreNote; index++) {
+           if(notes[index]>=10)  {
+            noteSupMoy[nbreVal++]=notes[index];
+           }     
+       }
+
     }
 }
